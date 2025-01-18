ec12fa8b7749b95c3440a585697cc27f77540374
Ahmed Ali <ahmedcali1984@gmail.com>
2025-01-18 11:44:04 +0300

    Add Comments to Help Readers

--- main.c
+++ main.c
@@ -253,22 +253,34 @@ void FreeBoard(CellPool _Board[BOARD_ROWS][BOARD_COLS]) {
 
 // NOTE: Main Function
 int main(void) {
+    // Allocate Memory For Grid
     Grid *grid = grid_alloc();
+
+    // Sudoku Grid as a Text File
     const char *file_path = "data/grid1.txt";
+
+    // Read The Sudoku text File and Load it in to the Grid
     if(!read_file(file_path, grid)) {
         return 1;
     }
-
+    
+    // Initialize Board , Load Grid into Board 
     InitBoard(grid);
+
+    // Print Initial Board 
     PrintBoard(Board);
 
+    // Search For Valid Numbers for Sudoku Cell and Populate them 
     if(!Search(Board)) {
+
+        // Exit if failed and Free Memories Allocated 
         printf("InValid Board.\n");
         FreeBoard(Board);
         grid_dealloc(grid);
         return 1;
     }
 
+    // Print Solved Board 
     printf("Board Solved.\n");
     PrintBoard(Board);
 
